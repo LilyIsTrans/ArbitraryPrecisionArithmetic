24bc1e606e435d6e7f8491aa8405f4e73681b188
Rilazy <35211613+Rilazy@users.noreply.github.com>
2022-10-02 12:33:43 -0400

    Setup basic project structure

---
+++ ArbitraryPrecisionArithmetic/src/apa.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+constexpr unsigned int BITS_PER_UINT = sizeof(unsigned long) * 8;
+
+using uint = unsigned int;
+using sint = int;
+
+namespace apa
+{
+	struct BigInt
+	{
+		
+	};
+}
